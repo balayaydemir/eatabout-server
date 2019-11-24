dba9531ead767a496807779f97a4d74f649368f4
balayaydemir <balayaydemir@gmail.com>
2019-11-24 12:09:31 -0800

    photo upload with filesize and mimetype done

--- src/upload/upload-router.js
+++ src/upload/upload-router.js
@@ -1,23 +1,46 @@
 const express = require('express');
 const { requireAuth } = require('../middleware/jwt-auth');
 const multer = require('multer');
+const path = require('path');
 
 
 const uploadRouter = express.Router();
+
+
 let storage = multer.diskStorage({
   destination: function(req, file, callback) {
     callback(null, __dirname + '/../../public/uploads');
   },
   filename: function(req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now());
+    callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
 });
 
+let checkFileType = (file, callback) => {
+  const filetypes = /jpeg|jpg|png|gif/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return callback(null, true);
+  } else {
+    callback('Error: Images Only');
+  }
+};
+
 uploadRouter.post('/', requireAuth, function(req, res) {
-  let upload = multer({ storage: storage }).single('photo_upload');
+  let upload = multer({ 
+    storage: storage,
+    limits: {fileSize: 1000000},
+    fileFilter: (req, file, callback) => {
+      checkFileType(file, callback);
+    } 
+  }).single('photo_upload');
   upload(req, res, function(err) {
     if (err) {
-      return res.end('cannot upload');
+      return res.status(400).json({
+        error: 'Image too large - please select an image that is 1MB or less'
+      });
     }
     res.json(req.file.filename);
   });
