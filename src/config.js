c5658143365de578c2cb82d1de6f91f92b9a00a9
balayaydemir <balayaydemir@gmail.com>
2019-11-24 21:46:46 -0800

    fixes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://eatabout-admin@localhost/eatabout',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://eatabout-admin@localhost/eatabout',
   CLIENT_ORIGIN: 'https://eatabout-app.balayaydemir.now.sh',
   JWT_SECRET: process.env.JWT_SECRET || '8854829f-fd62-4b0b-8f92-66ae25a53a16',
 };
