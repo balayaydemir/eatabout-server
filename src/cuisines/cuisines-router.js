caf4600543069c3c77a652ab702f65716d796956
balayaydemir <balayaydemir@gmail.com>
2019-11-24 21:21:02 -0800

    finished reporting feature

--- src/cuisines/cuisines-router.js
+++ src/cuisines/cuisines-router.js
@@ -17,4 +17,17 @@ cuisinesRouter
       .catch(next);
   });
 
+cuisinesRouter
+  .route('/report')
+  .get(requireAuth, (req, res, next) => {
+    CuisinesService.getCuisineChartData(
+      req.app.get('db'),
+      req.user.id
+    )
+      .then(cuisines => {
+        res.json(cuisines);
+      })
+      .catch(next);
+  });
+
 module.exports = cuisinesRouter;
--- src/cuisines/cuisines-service.js
+++ src/cuisines/cuisines-service.js
@@ -3,6 +3,26 @@ const CuisinesService = {
     return db
       .from('cuisines')
       .select('*');
+  },
+  getCuisineChartData(db, user_id) {
+    return db
+      .from('user_restaurants AS ur')
+      .where('user_id', user_id)
+      .select(
+        'cus.cuisine_name'
+      )
+      .count('cus.cuisine_name AS count')
+      .join(
+        'restaurants AS res',
+        'ur.restaurant_id',
+        'res.id'
+      )
+      .join(
+        'cuisines AS cus',
+        'res.cuisine',
+        'cus.id'
+      )
+      .groupBy('cus.cuisine_name');
   }
 };
 
